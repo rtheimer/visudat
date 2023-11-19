13b169c8badfd167880082c4518fc9146ca9d16c
Roy Theimer <roy.theimer@gmail.com>
2023-11-19 12:29:10 +0100

    project upload

---
+++ powervue/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PowervueConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "powervue"
---
+++ powervue/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path, include
+from . import views
+
+urlpatterns = [
+    path("", views.index),
+    
+
+
+]
