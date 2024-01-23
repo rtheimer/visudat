05b2a0ebba713fc3bc6a12e36d000c8b1356a3dd
Roy Theimer <roy.theimer@gmail.com>
2024-01-23 19:18:47 +0100

    removed folder powervue

--- powervue/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PowervueConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "powervue"
--- powervue/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, include
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    
-
-
-]
