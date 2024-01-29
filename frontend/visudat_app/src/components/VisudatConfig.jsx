e939ad08f418e40f022e4d4c13606008f9ed9595
Roy Theimer <roy.theimer@gmail.com>
2024-01-29 22:08:41 +0100

    chore: Make API host, protocol, and port dynamic in config
    Refactor the 'config.js' file to dynamically determine the API's host, protocol, and port based on the current window location. This allows for better flexibility and adaptability in different environments.

--- frontend/visudat_app/src/components/VisudatConfig.jsx
+++ frontend/visudat_app/src/components/VisudatConfig.jsx
@@ -1,2 +1,14 @@
-export const apiBaseUrl = "http://solar.orca-grp.de"; // without trailing slash
-export const apiPort = "41081";
+// config.js
+
+// Define the API protocol based on the current window location (http or https)
+export const apiProtocol = window.location.protocol;
+
+// Define the API base URL based on the current window location's hostname (e.g., domain or IP address)
+export const apiBaseUrl = window.location.hostname;
+
+// Define the API port based on the current window location's port
+// Note: This is a conditional assignment for development and build environments
+// In the build environment, use the dynamic port from the window.location.port
+// In the development environment, use a specific port (e.g., 8000)
+export const apiPort = window.location.port; // build
+//export const apiPort = 8000; // development
