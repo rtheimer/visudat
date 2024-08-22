ef4b036564c7d32e99dc7de0446894365110f073
Roy Theimer <roy.theimer@gmail.com>
2024-08-22 16:55:28 +0200

    Added Authentication

--- visudash/views.py
+++ visudash/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def index(request):
