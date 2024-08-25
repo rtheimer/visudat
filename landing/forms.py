bee2a603cc7fccc8fea1d6bd856e8566e14ad62d
Roy Theimer <roy.theimer@gmail.com>
2024-08-25 11:36:39 +0200

    Add placeholder

--- landing/forms.py
+++ landing/forms.py
@@ -4,5 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class VisudatAuthenticationForm(AuthenticationForm):
 
-    username = forms.CharField(label="Benutzer", widget=forms.TextInput(attrs={}))
-    password = forms.CharField(label="Passwort", widget=forms.TextInput(attrs={}))
+    username = forms.CharField(
+        label="Benutzer", widget=forms.TextInput(attrs={"placeholder": "Benutzername"})
+    )
+    password = forms.CharField(
+        label="Passwort", widget=forms.PasswordInput(attrs={"placeholder": "Passwort"})
+    )
