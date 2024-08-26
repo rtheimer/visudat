7e6fdc4dd5b6bd618b61d023262b5c64fc2c7bdd
Roy Theimer <roy.theimer@gmail.com>
2024-08-26 14:57:12 +0200

    Set AUTH_USER_MODEL

--- visudat/settings.py
+++ visudat/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # other apps
     "accounts",
     "landing",
     "visudash",
@@ -85,6 +84,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+# The Customized User Model
+AUTH_USER_MODEL = "accounts.CustomUser"
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
