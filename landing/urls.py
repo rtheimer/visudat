343c39dac928869db50f781c7b3f12c0c029443a
Roy Theimer <roy.theimer@gmail.com>
2024-08-26 09:23:55 +0200

    Created App Namespaces

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import LoginView
 
+app_name = "landing"
+
 urlpatterns = [
     path("", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- visudash/urls.py
+++ visudash/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from .views import DashboardView
 
+app_name = "dashboard"
+
 urlpatterns = [path("", DashboardView.as_view(), name="dashboard")]
