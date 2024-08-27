e557d6bcc5ad88c3429deab97c112bde58f3d5e1
Roy Theimer <roy.theimer@gmail.com>
2024-08-27 09:59:28 +0200

    Fixed next_url
    Pointed next_url to get("next")

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
 from .forms import VisudatAuthenticationForm
 from django.contrib.auth import authenticate, login
 
@@ -20,6 +21,10 @@ class LoginView(FormView):
         if user is not None:
             # If user is authenticated, log them in
             login(self.request, user)
+            # Check if 'next' parameter is present in the URL
+            next_url = self.request.GET.get("next")
+            if next_url:
+                return HttpResponseRedirect(next_url)
             return super().form_valid(form)
         else:
             return self.form_invalid(form)
