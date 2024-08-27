2aee487565072783d7bfba8033e39fadaf88691f
Roy Theimer <roy.theimer@gmail.com>
2024-08-27 09:53:20 +0200

    Add RegisterUser class

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.conf import django
+from django.contrib.auth import re
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class RegisterUser(LoginRequiredMixin, FormView):
+    login_url = "/"
+    redirect_field_name = "next"
+    template_name = "accounts/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("accounts:register")
+
+    def form_valid(self, form):
+        form.save()
+        # Add a success message
+        messages.success(self.request, "Registration successful!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # When the form is invalid, it will automatically pass the form with errors
+        return self.render_to_response(self.get_context_data(form=form))
